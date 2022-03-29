7ea73da8fef9bf24579ddf0540cb0f21dd9bdcce
Emily Steck <12025768+emilysteck@users.noreply.github.com>
2022-03-28 19:01:51 -0500

    Initial Sample Scene and ReadCSV script

---
+++ Assets/ReadCSV.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCSV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReadCSVFile();
+    }
+
+    void ReadCSVFile()
+    {
+        StreamReader strReader = ne StreamReader(""); //need to add file location
+        bool endOfFile = false;
+        while(!endOfFile){
+            string data_String = trReader.ReadLine();
+            if(data_String == null){
+                endOfFile = true;
+                break;
+            }
+            var data_values = data_String.Split(',');
+            Debug.Log(data_values[0].ToString() + " " + data_values[1].ToString() + " " + data_values[2].ToString() + " " + data_values[3].ToString())
+        }
+    }
+}
